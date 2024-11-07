dd3e176f7db0d9b3402f4bcf7843129db163a509
Aim-ez <154635991+Aim-ez@users.noreply.github.com>
2024-11-07 00:12:11 -0600

    Trigger expo build version 1.0.0

--- plot-line/constants/URL.js
+++ plot-line/constants/URL.js
@@ -1,2 +1,7 @@
 const HostURL = "http://192.168.100.210:3000"; // CHANGE IF IP ADDRESS CHANGES
 module.exports = { HostURL };
+
+//Honeslty no idea if this is going to work with
+//a production build?? 
+//Should probs also do a devlopment build trigger
+//in case.
